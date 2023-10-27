bf6ba48120d495bbea89e2a2946e1f07fe8038be
Jeremiah <jeremiahjacob261@gmail.com>
2023-10-27 10:35:38 +0100

    wc5

--- pages/user/index.js
+++ pages/user/index.js
@@ -3,9 +3,10 @@ import TextField from '@mui/material/TextField';
 import { Button,Stack } from '@mui/material';
 import { wallet } from '@/crypto/wc'
 export default function Home() {    
-    const [address, setAddress] = useState(localStorage.getItem('address'));
+    const [addresst, setAddress] = useState('');
     const [amount, setAmount] = useState(0);
     const [reciept, setReciept] = useState(null);
+    const [authed, setAuthed] = useState(false);
     const sendTRX = async () => {
         let transaction = {
             to: reciept,
@@ -21,9 +22,27 @@ export default function Home() {
             console.log('signTransaction:' + error);
           }
      };
+     useEffect(() => { 
+      const checkAuth = async () => {
+        try {
+          const { address } = await wallet.checkConnectStatus();
+          console.log(address);
+          setAddress(address)
+          if(address){
+            setAuthed(true);
+            alert("Connected")
+          }else{
+            setAuthed(false);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      checkAuth();
+     }, [authed]);
     return (
         <Stack direction='column' sx={{padding:'12px',background:'#FFFFFF'}} spacing={3}>
-            <h1 style={{color:'black'}}>user address :{address}</h1>
+            <h1 style={{color:'black'}}>user address :{addresst}</h1>
             <TextField value={amount} placeholder='amouunt' onChange={(e)=>{ setAmount(e.target.value) }}/>
             <TextField value={reciept} placeholder='reciever' onChange={(e)=>{ setReciept(e.target.value) }}/>
             <Button onClick={()=>{ sendTRX()}}>sendTRX</Button>
