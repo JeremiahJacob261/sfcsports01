50af4263b3a251e02f9c9c6d3d5104040a5a3d33
Jeremiah <jeremiahjacob261@gmail.com>
2023-10-30 08:55:18 +0100

    cv

--- pages/dashboard/account/index.js
+++ pages/dashboard/account/index.js
@@ -2,7 +2,7 @@ import HomeBottom from '../../UIComponents/bottomNav';
 export default function Account() {
     return (
         <div className='backgrounds'>
-            <h1>Account</h1>
+            <h1>Account Now</h1>
             <HomeBottom />
         </div>
     )
