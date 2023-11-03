89cf260bade041cb213da7f2c6330c102d15d350
Jeremiah <jeremiahjacob261@gmail.com>
2023-11-04 00:07:52 +0100

    best 1.6

--- pages/dashboard/account/index.js
+++ pages/dashboard/account/index.js
@@ -14,15 +14,19 @@ import Swapic from '@/pages/UIComponents/dialogs/swapic';
 export default function Account({ users }) {
     //accounts-balanace
     function AccountsBalance() {
+        let balanace = users.balance ?? 0;
         return (
             <Stack style={{ width: '100%' }} justifyContent='center' alignItems='center'>
                 <Stack sx={{ width: '100%', padding: '8px' }}>
-                    <p style={{ fontWeight: '500', fontSize: '15px' }}>Wallet</p>
+                    <Stack direction='row' justifyContent='start' alignItems='center' spacing={1}>
+                        <Icon icon="mdi:cash" width={24} height={24} />
+                        <p style={{ fontWeight: '500', fontSize: '15px' }}>Wallet</p>
+                    </Stack>
                     <Divider sx={{ background: 'white', color: 'white' }} />
                 </Stack>
                 {/* accountinfoandBalance */}
                 <Stack className='accountinfo' justifyContent='center' alignItems='center' spacing={1}>
-                    <p style={{ fontWeight: '600', fontSize: '24px', color: '#ac915fd2' }}>$ {users.balance}</p>
+                    <p style={{ fontWeight: '600', fontSize: '24px', color: '#ac915fd2' }}>$ {balanace.toFixed(2)}</p>
                     <p style={{ fontWeight: '300', fontSize: '12px' }}>Account Balance</p>
                     <motion.p onClick={() => {
                         router.push('/dashboard/fund')
@@ -38,7 +42,7 @@ export default function Account({ users }) {
                         whileHover={{ background: '#573b41' }}
                         style={{ fontWeight: '500', fontSize: '12px', color: '#C61F41', padding: '8px', background: 'white', width: '100%', textAlign: 'center', cursor: 'pointer' }}>
                         WITHDRAW</motion.p>
-                        <motion.p onClick={() => {
+                    <motion.p onClick={() => {
                         router.push('/dashboard/transactions')
                     }}
                         whileTap={{ background: '#ac915fd2', scale: '1.05' }}
@@ -61,6 +65,84 @@ export default function Account({ users }) {
         );
     }
     //end of language
+
+    //start of bets
+    function Bets() {
+        return (
+            <Stack style={{ width: '100%' }} justifyContent='start' alignItems='center'>
+                <Stack sx={{ width: '100%', padding: '8px' }} spacing={1}>
+                    <Stack direction='row' justifyContent='start' alignItems='center' spacing={1}>
+                        <Icon icon="material-symbols:casino-outline-sharp" width={24} height={24} />
+                        <p style={{ fontWeight: '500', fontSize: '15px' }}>Bets</p>
+                    </Stack>
+                    <Divider sx={{ background: 'white', color: 'white' }} />
+                </Stack>
+                <Stack className='accountinfo'>
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ padding: '8px' }}>
+                        <Stack direction='row' alignItems='center' justifyContent='center' spacing={1}>
+                            <Icon icon="mdi:receipt-text-pending" /> 
+                        <p>Pending Bets</p></Stack>
+                        <p style={{ color:'rgba(245,186,79,1)'}}>0</p>
+                    </Stack>
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ padding: '8px' }}>
+                    <Stack direction='row' alignItems='center' justifyContent='center' spacing={1}>
+                            <Icon icon="mdi:store-complete-outline" /> 
+                        <p>Settled Bets</p></Stack>
+                        <p style={{ color:'green'}}>0</p>
+                    </Stack>
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ padding: '8px' }}>
+                    <Stack direction='row' alignItems='center' justifyContent='center' spacing={1}>
+                            <Icon icon="mdi:cup-full" /> 
+                        <p>Total Bets</p></Stack>
+                        <p style={{ color:'grey'}}>0</p>
+                    </Stack>
+                    <Divider sx={{ background: 'rgba(245,186,79,1)', color: 'rgba(245,186,79,1)' }} />
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ padding: '8px' }}>
+                        <p>Total Wins</p>
+                        <p style={{ color:'blue'}}>0</p>
+                        </Stack>
+                </Stack>
+            </Stack>
+        );
+    }
+    //end of bets
+
+    //start of security
+    function Security() {
+        return(
+            <Stack style={{ width: '100%' }} justifyContent='start' alignItems='center'>
+                <Stack sx={{ width: '100%', padding: '8px' }} spacing={1}>
+                    <Stack direction='row' justifyContent='start' alignItems='center' spacing={1}>
+                        <Icon icon="mdi:security" width={24} height={24} />
+                        <p style={{ fontWeight: '500', fontSize: '15px' }}>Security</p>
+                    </Stack>
+                    <Divider sx={{ background: 'white', color: 'white' }} />
+                </Stack>
+                <Stack className='accountinfo'>
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ padding: '8px' }}>
+                    <Stack direction='row' alignItems='center' justifyContent='center' spacing={1}>
+                            <Icon icon="ic:baseline-password" /> 
+                        <p>Change Password</p></Stack>
+                        <Icon icon="mdi:chevron-right" width={24} height={24} />
+                    </Stack>
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ padding: '8px' }}>
+                    <Stack direction='row' alignItems='center' justifyContent='center' spacing={1}>
+                            <Icon icon="mdi-light:email" /> 
+                        <p>Change Email</p></Stack>
+                        <Icon icon="mdi:chevron-right" width={24} height={24} />
+                    </Stack>
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' sx={{ padding: '8px' }}>
+                    <Stack direction='row' alignItems='center' justifyContent='center' spacing={1}>
+                            <Icon icon="solar:key-broken" /> 
+                        <p>Change Transaction Password</p></Stack>
+                        <Icon icon="mdi:chevron-right" width={24} height={24} />
+                    </Stack>
+                </Stack>
+            </Stack>
+        
+        )
+    }
+    //end of security
     const router = useRouter();
     return (
         <div className='backgrounds'>
@@ -89,6 +171,8 @@ export default function Account({ users }) {
                     </Stack>
                 </Stack>
                 <AccountsBalance />
+                <Bets />
+                <Security />
             </Stack>
             <HomeBottom />
         </div>
@@ -110,15 +194,11 @@ export async function getServerSideProps({ req }) {
         throw new Error('User is not authenticated.')
     }
     // returns user information
-    try{
-        let { data: user, error } = await supabase.auth.getUser()
-    console.log(user.user.user_metadata)
-    console.log(error)
-    }catch(e){
-        console.log(e)
-    }
-    
+
+
     try {
+        let { data: user, error: err } = await supabase.auth.getUser()
+        console.log(user.user.user_metadata)
         const { data, error } = await supabase
             .from('users')
             .select('*')
