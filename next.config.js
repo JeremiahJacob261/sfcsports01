7407982f2e2b62fcb88c0a1d7b6db12b3e37e6e6
Jeremiah <jeremiahjacob261@gmail.com>
2023-11-12 16:49:34 +0100

    pwa added ,best 2.4

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
 const nextConfig = {
   reactStrictMode: true,
   images: {
   domains: ['aidkzrgsgrfotjiouxto.supabase.co','firebasestorage.googleapis.com','media-1.api-sports.io','media-2.api-sports.io','media-3.api-sports.io','media.api-sports.io','media-4.api-sports.io','upload.wikimedia.org'],
 },
-
+swcMinify: true,     // Enable SWC minification for improved performance
+compiler: {
+  removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+},
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
