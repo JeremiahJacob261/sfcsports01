129c369be4e38da3729e9e1a78dbd429341459fa
Jeremiah <jeremiahjacob261@gmail.com>
2023-12-15 07:56:45 +0100

    added sfc build

--- pages/dashboard/promotion/claim_signup_bonus/index.js
+++ pages/dashboard/promotion/claim_signup_bonus/index.js
@@ -55,7 +55,7 @@ export default function Claim() {
         <div className="backgrounds" style={{ height: "99vh" }}>
             <Stack className='headers' direction="row" alignItems='center' sx={{ padding: '8px', width: '100%' }} spacing={1}>
                 <Icon icon="ic:sharp-arrow-back" width={24} height={24} onClick={() => {
-                    router.push('/dashboard/account/promotion')
+                    router.push('/dashboard/promotion')
                 }} />
                 <p className="text-sm text-gray-500">Claim Sign Up Bonus</p>
             </Stack>
