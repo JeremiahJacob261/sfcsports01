6470eabf913718ac78818eacdcee466d171a8943
Jeremiah <jeremiahjacob261@gmail.com>
2023-12-26 22:04:06 +0100

    BEST 7.3

--- pages/dashboard/history/index.js
+++ pages/dashboard/history/index.js
@@ -9,85 +9,18 @@ import { supabase } from '@/pages/api/supabase';
 export default function History() {
     const router = useRouter();
     const [data, setData] = useState({});
+    const [ref,setRef] = useState('')
     const [noti, setNoti] = useState(false);
     //hisstory-dx
 
-    function HistoryDx() {
-        const [open, setOpen] = useState(false);
-        const handleOpen = () => setOpen(true);
-        const handleClose = () => setOpen(false);
-      
-        return (
-          <div >
-            <Icon icon="fa6-solid:expand" width={24} height={24} className='iconbtn' style={{ color: 'white' }}
-              onClick={handleOpen}
-            />
-            <Modal
-              aria-labelledby="transition-modal-title"
-              aria-describedby="transition-modal-description"
-              open={open}
-              onClose={handleClose}
-              closeAfterTransition
-              slots={{ backdrop: Backdrop }}
-              slotProps={{
-                backdrop: {
-                  timeout: 500,
-                },
-              }}
-            >
-              <Fade in={open}>
-                <Box className='history'>
-                  <Typography id="transition-modal-title" variant="h6" component="h2" sx={{ width:"100%",textAlign:'center',color:"#e4264c"}}>
-                    Transaction History
-                  </Typography>
-                  <Stack spacing={2} sx={{ width: '100%' }} direction="column" justifyContent='center'>
-                 
-                    <Stack direction="row" justifyContent='space-between' alignItems='center' sx={{ padding:'8px'}}> 
-         <p style={{ color:'rgba(245,186,79,1)'}}>Transaction Type</p>
-      <p>{data.type ?? ''}</p>
-                    </Stack>
-                    <Stack direction="row" justifyContent='space-between' alignItems='center' sx={{ padding:'8px'}}> 
-        <p style={{ color:'rgba(245,186,79,1)'}}> Status</p>
-      <p>{data.status}</p>
-                    </Stack>
-                    <Stack direction="row" justifyContent='space-between' alignItems='center' sx={{ padding:'8px'}}> 
-        <p style={{ color:'rgba(245,186,79,1)'}}>Amount</p>
-      <p>{data.amount} USDT</p>
-                    </Stack>
-                    <Stack direction="row" justifyContent='space-between' alignItems='center' sx={{ padding:'8px'}}> 
-        <p style={{ color:'rgba(245,186,79,1)'}}> Description</p>
-      <p>{data.description}</p>
-                    </Stack>
-                    <Stack direction="row" justifyContent='space-between' alignItems='center' sx={{ padding:'8px'}}> 
-        <p style={{ color:'rgba(245,186,79,1)'}}>Payment Method</p>
-      <p>{data.payment}</p>
-                    </Stack>
-      
-                    <Stack direction="row" justifyContent='space-between' alignItems='center' sx={{ padding:'8px'}}> 
-        <p style={{ color:'rgba(245,186,79,1)'}}>Time</p>
-      <p>{data.time}</p>
-                    </Stack>
-                  </Stack>
-      
-                  <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className='cancelbrn'>
-                    <Icon icon="iconoir:cancel" onClick={handleClose} width={24} height={24} style={{ color: 'white' }} />
-      
-                  </motion.div>
-                </Box>
-              </Fade>
-            </Modal>
-          </div>
-        );
-      }
-
-    //history-dx
     useEffect(()=>{
-            console.log('started')
+            console.log('started');
+            setRef(localStorage.getItem('signRef'));
         const getNoti = async () =>{
             const { data,error } = await supabase
             .from('activa')
             .select('*')
-            .eq('username',localStorage.getItem('signNames'))
+            .or(`username.eq.${localStorage.getItem('signNames')},code.eq.${localStorage.getItem('signRef')}`)
             .order('id', { ascending: false })
             setNoti(data);
             console.log(data)
@@ -135,12 +68,7 @@ export default function History() {
                             <p><p style={{ fontWeight:'bold',color:'greenyellow'}}>{item.type}</p> has just signed up with your Referral Link</p>
                             <p style={{ color: 'white' }}>{fullDay}</p>
                         </Stack>
-                        <Stack justifyContent='center' alignItems="center">
-                            <HistoryDx onClick={()=>{
-                                setData(infos)
-                            
-                            }}/>
-                        </Stack>
+                       
                     </Stack>
                             )
                           } else if(item.code === 'bet-cancellation'){
@@ -161,15 +89,11 @@ export default function History() {
                             <p style={{ color: 'white' }}>{fullDay}</p>
                             <p style={{ color: 'white',fontSize:'11px' }}>If you did not Request for this Bet Cancellation, please contact Customer Care</p>
                         </Stack>
-                        <Stack justifyContent='center' alignItems="center">
-                            <HistoryDx onClick={()=>{
-                                setData(infos)
-                            
-                            }}/>
-                        </Stack>
                     </Stack>
                             )
                           }else if(item.type === 'bonus'){
+                          if(item.code === localStorage.getItem('signRef')){
+
                             let infos = {
                                 type:'bonus',
                                 amount:item.amount,
@@ -182,18 +106,29 @@ export default function History() {
                             return(
                                 <Stack className='bottomnav' direction='row' key={item.id} justifyContent='space-between' alignItems='center' sx={{ border: '1px solid #C61F41', maxWidth: '90vw', minWidth: '80vw', borderRadius: '5px' }}>
                         <Stack>
+                            <p style={{ fontWeight:'bold',color:'greenyellow'}}>You just recieved a {item.type} from {item.username}</p>
+                            <p>{item.amount} USDT</p>
+                            <p style={{ color: 'white' }}>{fullDay}</p>
+                        </Stack>
+                       
+                    </Stack>
+                            )
+
+                          }else{
+                          if(item.code.length > 0){
+                            return(
+                                <Stack className='bottomnav' direction='row' key={item.id} justifyContent='space-between' alignItems='center' sx={{ border: '1px solid #C61F41', maxWidth: '90vw', minWidth: '80vw', borderRadius: '5px' }}>
+                        <Stack>
                             <p style={{ fontWeight:'bold',color:'greenyellow'}}>You just recieved a {item.type}</p>
                             <p>{item.amount} USDT</p>
                             <p style={{ color: 'white' }}>{fullDay}</p>
                         </Stack>
-                        <Stack justifyContent='center' alignItems="center">
-                            <HistoryDx onClick={()=>{
-                                setData(infos)
-                            
-                            }}/>
-                        </Stack>
+                       
                     </Stack>
                             )
+                          } 
+                          
+                          }
                           } else if(item.code === 'usdtdepositsuccess'){
                             let infos = {
                                 type:'deposit',
@@ -211,12 +146,6 @@ export default function History() {
                                     <p>{item.amount} USDT</p>
                                     <p style={{ color: 'white' }}>{fullDay}</p>
                                 </Stack>
-                                <Stack justifyContent='center' alignItems="center">
-                                    <HistoryDx onClick={()=>{
-                                setData(infos)
-                            
-                            }}/>
-                                </Stack>
                             </Stack>
                             )
                           }else if(item.code === 'bet-placed'){
@@ -233,14 +162,9 @@ export default function History() {
                                 <Stack className='bottomnav' direction='row' key={item.id} justifyContent='space-between' alignItems='center' sx={{ border: '1px solid #C61F41', maxWidth: '90vw', minWidth: '80vw', borderRadius: '5px' }}>
                                 <Stack>
                                     <p style={{ fontWeight:'bold',color:'greenyellow'}}>Your Bet was placed Successfully</p>
+                                    <p>{item.type}</p>
                                     <p>{item.amount} USDT</p>
                                     <p style={{ color: 'white' }}>{fullDay}</p>
-                                </Stack>
-                                <Stack justifyContent='center' alignItems="center">
-                                    <HistoryDx onClick={()=>{
-                                setData(infos)
-                            
-                            }}/>
                                 </Stack>
                             </Stack>
                             )
@@ -262,15 +186,10 @@ export default function History() {
                                     <p>{item.amount} USDT</p>
                                     <p style={{ color: 'white' }}>{fullDay}</p>
                                 </Stack>
-                                <Stack justifyContent='center' alignItems="center">
-                                    <HistoryDx onClick={()=>{
-                                setData(infos)
-                            
-                            }}/>
-                                </Stack>
                             </Stack>
                             )
                           }else{
+                           if(item.code === 'usdtdepositfailed'){
                             let infos = {
                                 type:'Broadcast',
                                 amount:'',
@@ -287,14 +206,9 @@ export default function History() {
                                     <p>{item.amount} USDT</p>
                                     <p style={{ color: 'white' }}>{fullDay}</p>
                                 </Stack>
-                                <Stack justifyContent='center' alignItems="center">
-                                    <HistoryDx onClick={()=>{
-                                setData(infos)
-                            
-                            }}/>
-                                </Stack>
                             </Stack>
                             )
+                           }
                           }
                         })
                     }
