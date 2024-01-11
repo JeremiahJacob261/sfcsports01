db005822dd5193c1c29c2ab6150eb408608287ba
Jeremiah <jeremiahjacob261@gmail.com>
2024-01-11 04:10:17 +0100

    vip wahala 2

--- pages/api/vip.js
+++ pages/api/vip.js
@@ -1,15 +1,15 @@
 import { NextResponse } from 'next/server';
 import { supabase } from '../../pages/api/supabase';
 export default async function handler(req, res) {
-  const body = req.body;
-  let username = body.username;
-  
-   try{
+    const body = req.body;
+    let username = body.username;
+
+    try {
         //all
-        const { data:user, error:uerror } = await supabase
-        .from('users')
-        .select('*')
-        .eq('username',username)
+        const { data: user, error: uerror } = await supabase
+            .from('users')
+            .select('*')
+            .eq('username', username)
         let users = user[0];
         try {
 
@@ -38,25 +38,26 @@ export default async function handler(req, res) {
                     'refer': user[0].newrefer,
                     'firstd': true
                 });
-                console.log(count)
+            console.log(count)
             let refCount = count;
             let vipl = (users.totald < 50 || count < 3) ? '1' : (users.totald < 100 || count < 5) ? '2' : (users.totald < 200 || count < 8) ? '3' : (users.totald < 300 || count < 12) ? '4' : (users.totald < 500 || count < 15) ? '5' : (users.totald < 1000 || count < 20) ? '6' : '7';
-           
+
             let viplevel = (users.totald < 50 || count < 3) ? '1' : (users.totald < 100 || count < 5) ? '2' : (users.totald < 200 || count < 8) ? '3' : (users.totald < 300 || count < 12) ? '4' : (users.totald < 500 || count < 15) ? '5' : (users.totald < 1000 || count < 20) ? '6' : '7';
             let rprogress = (parseInt(users.totald) / parseInt(viplimit[vipl])) * 100;
             //tests
-// console.log(users.totald)
+            // console.log(users.totald)
             //end
             let cprogress = (parseInt(count) / parseInt(vipclimit[vipl])) * 100;
             let c1 = (Number(((parseInt(count) / parseInt(vipclimit[vipl])) * 100).toFixed(2)) > 100) ? 100 : Number(((parseInt(count) / parseInt(vipclimit[vipl])) * 100).toFixed(2));
             let r1 = (Number(((parseInt(users.totald) / parseInt(viplimit[vipl])) * 100).toFixed(2)) > 100) ? 100 : Number(((parseInt(users.totald) / parseInt(viplimit[vipl])) * 100).toFixed(2));
             console.log(rprogress, cprogress, refCount, viplevel)
-        res.status(200).json({ status: 'success',refCount:parseFloat(refCount) ?? 0,viplevel:parseFloat(viplevel) ?? 0,rprogress:parseFloat(rprogress.toFixed(2)),cprogress:parseFloat(cprogress.toFixed(2)),c1:parseFloat(c1),r1:parseFloat(r1) })
-   
+            res.status(200).json({ status: 'success', refCount: parseFloat(refCount) ?? 0, viplevel: parseFloat(viplevel) ?? 0, rprogress: parseFloat(rprogress.toFixed(2)), cprogress: parseFloat(cprogress.toFixed(2)), c1: parseFloat(c1), r1: parseFloat(r1) })
+
         } catch (e) {
             console.log(e)
-            res.status(500).json({ status: 'failed',message:e })
-        }}catch(e){
-    res.status(500).json({ status: 'failed',message:e })
-   }
-  }
+            res.status(500).json({ status: 'failed', message: e })
+        }
+    } catch (e) {
+        res.status(500).json({ status: 'failed', message: e })
+    }
+}
--- pages/dashboard/account/index.js
+++ pages/dashboard/account/index.js
@@ -617,18 +617,51 @@ export default function Account({ vips }) {
 export async function getServerSideProps(context) {
     const id = context.query.id;
     const { locale } = context;
+    const { data: user, error: uerror } = await supabase
+            .from('users')
+            .select('*')
+            .eq('username', id)
+        let users = user[0];
     try {
-        let test = await fetch('https://sfcsports01.com/api/vip', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ "username": id }),
-            cache: 'no-store'
-        }).then(data => {
-            return data.json();
-        })
-        console.log(test)
+        const viplimit = {
+            '1': 50,
+            '2': 100,
+            '3': 200,
+            '4': 300,
+            '5': 500,
+            '6': 1000,
+            '7': 5000
+        };
+        const vipclimit = {
+            '1': 3,
+            '2': 5,
+            '3': 8,
+            '4': 12,
+            '5': 15,
+            '6': 20,
+            '7': 500
+        };
+        const { count, error } = await supabase
+            .from('users')
+            .select('*', { count: 'exact', head: true })
+            .match({
+                'refer': user[0].newrefer,
+                'firstd': true
+            });
+        console.log(count)
+        let refCount = count;
+        let vipl = (users.totald < 50 || count < 3) ? '1' : (users.totald < 100 || count < 5) ? '2' : (users.totald < 200 || count < 8) ? '3' : (users.totald < 300 || count < 12) ? '4' : (users.totald < 500 || count < 15) ? '5' : (users.totald < 1000 || count < 20) ? '6' : '7';
+
+        let viplevel = (users.totald < 50 || count < 3) ? '1' : (users.totald < 100 || count < 5) ? '2' : (users.totald < 200 || count < 8) ? '3' : (users.totald < 300 || count < 12) ? '4' : (users.totald < 500 || count < 15) ? '5' : (users.totald < 1000 || count < 20) ? '6' : '7';
+        let rprogress = (parseInt(users.totald) / parseInt(viplimit[vipl])) * 100;
+        //tests
+        // console.log(users.totald)
+        //end
+        let cprogress = (parseInt(count) / parseInt(vipclimit[vipl])) * 100;
+        let c1 = (Number(((parseInt(count) / parseInt(vipclimit[vipl])) * 100).toFixed(2)) > 100) ? 100 : Number(((parseInt(count) / parseInt(vipclimit[vipl])) * 100).toFixed(2));
+        let r1 = (Number(((parseInt(users.totald) / parseInt(viplimit[vipl])) * 100).toFixed(2)) > 100) ? 100 : Number(((parseInt(users.totald) / parseInt(viplimit[vipl])) * 100).toFixed(2));
+        console.log(rprogress, cprogress, refCount, viplevel)
+        let test = { status: 'success', refCount: parseFloat(refCount) ?? 0, viplevel: parseFloat(viplevel) ?? 0, rprogress: parseFloat(rprogress.toFixed(2)), cprogress: parseFloat(cprogress.toFixed(2)), c1: parseFloat(c1), r1: parseFloat(r1) }
         return {
             props: {
                 vips: test,
