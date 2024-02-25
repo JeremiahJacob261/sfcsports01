9b43b5e44a70b8a0b1edd68bd29c4b896d224471
Jeremiah <jeremiahjacob261@gmail.com>
2024-02-25 10:19:42 +0100

    fix routing 2

--- pages/dashboard/referral/index.js
+++ pages/dashboard/referral/index.js
@@ -152,7 +152,7 @@ export default function Referral({ test }) {
 export async function getServerSideProps(context) {
     try {
         let users = context.query.name;
-        let test = await fetch('https://sfcsports01.com/api/referral', {
+        let test = await fetch('https://www.sfcsports01.com/api/referral', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
