8ab9f10d736938580c3baa4b13cc8cb0d1243b3a
Jeremiah <jeremiahjacob261@gmail.com>
2024-04-11 18:58:35 +0100

    to 1 usd

--- pages/dashboard/matchs/[id].js
+++ pages/dashboard/matchs/[id].js
@@ -83,8 +83,8 @@ export default function Matchs({ matc, user, test }) {
         alert('Please Input a Number')
         setDrop(false)
         return;
-      } else if (parseFloat(stake) < 5) {
-        alert('Minimum stake is 5 USDT')
+      } else if (parseFloat(stake) < 1) {
+        alert('Minimum stake is 1 USDT')
         setDrop(false)
         return;
       } else if (parseFloat(stake) > user.balance) {
