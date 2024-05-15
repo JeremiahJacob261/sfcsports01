08a1e1e72c18d45135c2254ccc740206dc5d944b
Jeremiah <jeremiahjacob261@gmail.com>
2024-05-15 23:03:34 +0100

    fix cors

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ const withPWA = require("next-pwa")({
   skipWaiting: true, // Skip waiting for service worker activation
 });
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3000' }, // Specify your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
   domains: ['aidkzrgsgrfotjiouxto.supabase.co','firebasestorage.googleapis.com','media-1.api-sports.io','media-2.api-sports.io','media-3.api-sports.io','media.api-sports.io','media-4.api-sports.io','upload.wikimedia.org'],
